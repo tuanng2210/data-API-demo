040998c438b42b654253255bcd173a2447a34dfe
Tuan Nguyen <tuanng2210@gmail.com>
2024-08-20 13:02:30 -0400

    phase3

--- src/main/java/com/example/demo_data_API/api/CustomerAPI.java
+++ src/main/java/com/example/demo_data_API/api/CustomerAPI.java
@@ -1,9 +1,6 @@
 package com.example.demo_data_API.api;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,27 +13,18 @@ import com.example.demo_data_API.repository.CustomersRepository;
 @RestController
 @RequestMapping("/api")
 public class CustomerAPI {
-    private List<Customer> customers;
-    CustomersRepository repo;
 
     @Autowired
-    public CustomerAPI() {
-        customers = new ArrayList<>();
-        customers.add(new Customer(1, "John Doe", "johndoe@example.com"));
-        customers.add(new Customer(2, "Jane Smith", "janesmith@example.com"));
-        customers.add(new Customer(3, "Alice Johnson", "alicej@example.com"));
-    }
+    CustomersRepository repo;
 
     @GetMapping("/customers")
     public Iterable<Customer> getAll() {
-        return customers;
+        return repo.findAll();
     }
 
     @GetMapping("/customers/{id}")
     public Optional<Customer> getCustomerById(@PathVariable("id") Long id) {
-        return customers.stream()
-                .filter(customer -> customer.getId() == id)
-                .findFirst();
+        return repo.findById(id);
     }
 
 }
--- src/main/java/com/example/demo_data_API/domain/Customer.java
+++ src/main/java/com/example/demo_data_API/domain/Customer.java
@@ -1,30 +1,36 @@
 package com.example.demo_data_API.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "CUSTOMERS")
 public class Customer {
-    @Id
+	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	long id;
 
+	@Column(name = "CUSTOMER_NAME")
 	String name;
-	 
+
 	String email;
 
-	public Customer(long id, String name, String email ){
+	String password;
+
+	public Customer(long id, String name, String email) {
 		this.id = id;
 		this.name = name;
 		this.email = email;
 	}
-	
+
 	public long getId() {
 		return id;
 	}
-	
+
 	public void setId(long id) {
 		this.id = id;
 	}
@@ -44,6 +50,13 @@ public class Customer {
 	public void setEmail(String email) {
 		this.email = email;
 
-	
-}
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
