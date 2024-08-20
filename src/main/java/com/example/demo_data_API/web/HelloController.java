9a0256943eb7d70b357e2f153890ee51bfb2d1ca
Tuan Nguyen <tuanng2210@gmail.com>
2024-08-20 11:48:51 -0400

    add /api route

--- src/main/java/com/example/demo_data_API/web/HelloController.java
+++ src/main/java/com/example/demo_data_API/web/HelloController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @GetMapping("/hello")
+    @GetMapping("/api")
     public String Hello(){
-        return "Hello World";
+        return "API is working";
     }
 }
