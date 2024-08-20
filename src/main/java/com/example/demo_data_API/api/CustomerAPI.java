9b2843f821e7405c3c7d7b2ead353ba2a42dbf42
Tuan Nguyen <tuanng2210@gmail.com>
2024-08-20 15:50:36 -0400

    dete endpoint working

--- src/main/java/com/example/demo_data_API/api/CustomerAPI.java
+++ src/main/java/com/example/demo_data_API/api/CustomerAPI.java
@@ -4,10 +4,13 @@ import java.net.URI;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,6 +47,25 @@ public class CustomerAPI {
                 .buildAndExpand(newCustomer.getId()).toUri();
         ResponseEntity<?> response = ResponseEntity.created(location).build();
         return response;
+    }
+
+    @PutMapping("/customers/{customerId}")
+    public ResponseEntity<?> putCustomer(
+            @RequestBody Customer newCustomer,
+            @PathVariable("customerId") long customerId) {
+        if (newCustomer.getId() != customerId || newCustomer.getName() == null ||
+                newCustomer.getEmail() == null) {
+            return ResponseEntity.badRequest().build();
+        }
+        newCustomer = repo.save(newCustomer);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/customers/{customerId}")
+    public ResponseEntity<?> deleteCustomerById(@PathVariable("customerId") long id) {
+
+        repo.deleteById(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
 }
