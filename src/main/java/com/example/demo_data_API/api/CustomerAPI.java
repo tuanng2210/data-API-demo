dd400d68caa1f488e677b3c9808d8caed21b0f0f
Tuan Nguyen <tuanng2210@gmail.com>
2024-08-20 11:28:17 -0400

    phase2

--- src/main/java/com/example/demo_data_API/api/CustomerAPI.java
+++ src/main/java/com/example/demo_data_API/api/CustomerAPI.java
@@ -2,19 +2,22 @@ package com.example.demo_data_API.api;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo_data_API.domain.Customer;
+import com.example.demo_data_API.repository.CustomersRepository;
 
 @RestController
-@RequestMapping("/customers")
+@RequestMapping("/api")
 public class CustomerAPI {
     private List<Customer> customers;
-    // CustomersRepository repo;
+    CustomersRepository repo;
 
     @Autowired
     public CustomerAPI() {
@@ -24,9 +27,21 @@ public class CustomerAPI {
         customers.add(new Customer(3, "Alice Johnson", "alicej@example.com"));
     }
 
-    @GetMapping
+    @GetMapping("/customers")
     public Iterable<Customer> getAll() {
         return customers;
+    }
+
+    // @GetMapping("/customers/{id}")
+    // public Optional<Customer> getCustomerById(@PathVariable Long id) {
+    //     return repo.findById(id);
+    // }
+
+    public Optional<Customer> getCustomerById(@PathVariable Long id) {
+        // Search for the customer by ID in the hardcoded list
+        return customers.stream()
+                .filter(customer -> customer.getId() == id)
+                .findFirst();
     }
 
 }
